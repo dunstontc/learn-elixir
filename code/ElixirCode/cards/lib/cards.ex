99c83d9fedb6edd2178e60df547f94a10bbbb31c
Clay Dunston <dunstontc@gmail.com>
2018-07-10 11:04:25 -0500

    pre-test

--- code/ElixirCode/cards/lib/cards.ex
+++ code/ElixirCode/cards/lib/cards.ex
@@ -16,6 +16,9 @@ defmodule Cards do
     #     List.flatten(deck)
     # end
 
+    @doc """
+    Returns a list of strings representing a deck of playing cards.
+    """
     def create_deck do
         values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"]
         suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -33,11 +36,26 @@ defmodule Cards do
         Enum.member?(deck, card)
     end
 
+    @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+    ## Examples
+    ```
+    iex> deck = Cards.create_deck
+    iex> {hand, deck} = Cards.deal(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+    ```
+    """
     def deal(deck, hand_size) do
         {head, _tail} = Enum.split(deck, hand_size)
         head
     end
 
+    @doc """
+    Saves a collection of cards to a file on the local machine.
+    """
     def save(deck, filename) do
         binary = :erlang.term_to_binary(deck)
         File.write(filename, binary)
@@ -52,6 +70,9 @@ defmodule Cards do
     #     end
     # end
 
+    @doc """
+    Loads a collection of cards from the local machine.
+    """
     def load(filename) do
         case File.read(filename) do
             {:ok, binary} -> :erlang.binary_to_term(binary)
