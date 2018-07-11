121e6ccc93bd1273312f75ae077709ec62568b56
Clay Dunston <dunstontc@gmail.com>
2018-07-10 20:49:59 -0500

    Aaand we've got identicons

--- code/ElixirCode/identicon/lib/identicon.ex
+++ code/ElixirCode/identicon/lib/identicon.ex
@@ -5,11 +5,28 @@ defmodule Identicon do
 
   def main(input) do
     input
-    |> hash_input
-    |> pick_color
-    |> build_grid
-    |> filter_odd_squares
-    |> build_pixel_map
+    |> hash_input()
+    |> pick_color()
+    |> build_grid()
+    |> filter_odd_squares()
+    |> build_pixel_map()
+    |> draw_image()
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
