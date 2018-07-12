d41857e9fbeb51cbba73f2a7c2dea724d082958e
Clay Dunston <dunstontc@gmail.com>
2018-07-12 14:01:13 -0500

    topic resources

--- code/ElixirCode/discuss/web/router.ex
+++ code/ElixirCode/discuss/web/router.ex
@@ -2,23 +2,28 @@ defmodule Discuss.Router do
   use Discuss.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", Discuss do
     pipe_through(:browser) # Use the default browser stack
 
-    get("/", TopicController, :index)
-    get("/topics/new", TopicController, :new)
-    post("/topics", TopicController, :create)
+    resources("/", TopicController)
+
+    # get("/", TopicController, :index)
+    # get("/topics/new", TopicController, :new)
+    # post("/topics", TopicController, :create)
+    # get("/topics/:id/edit", TopicController, :edit)
+    # put("/topics/:id", TopicController, :update)
+
   end
 
 
